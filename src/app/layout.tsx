53d13000b43dd3c99239a630c0c5553881210143
Viacheslav-ua <atph@ukr.net>
2024-01-30 20:44:26 +0200

    added heade and routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 
 import { cn } from "@/shared/ul/utils";
-import { AppHeader } from "@/widgets/app-header/app-header";
+import { AppProvider } from "./(auth)/_providers/app-provider";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -21,15 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en suppressHydrationWarning">
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
           fontSans.variable,
         )}
       >
-        <AppHeader />
-        {children}
+        <AppProvider>{children}</AppProvider>
       </body>
     </html>
   );
